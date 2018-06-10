dc72897a8f38359750eea757315953bd3e35b54b
lazy1993 <2280042554@qq.com>
2018-06-10 21:21:22 +0800

    change
    初始化flutter项目

---
+++ android/app/src/main/java/com/lazy1993/ffmusic/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lazy1993.ffmusic;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
